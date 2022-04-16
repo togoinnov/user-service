52b48e13066d6b61a15bbcae422faa6d086b9608
marc.atsou@gmail.com <marc.atsou@gmail.com>
2022-04-16 12:43:23 +0200

    Implement Adress and User Dtos

---
+++ src/main/java/com/togoinov/user/domain/api/AddressDto.java
@@ -0,0 +1,53 @@
+package com.togoinov.user.domain.api;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Value;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Builder(toBuilder = true)
+@Data
+@Value
+public class AddressDto {
+    @Nullable
+    UUID id;
+
+    @Schema(
+            example = "Avenue de la Libération prolongée"
+    )
+
+    @NonNull
+    @NotNull
+    String street;
+
+    @Schema(
+            example = "15"
+    )
+    @Nullable
+    String houseNumber;
+
+    @Schema(
+            example = "3174"
+    )
+    @Nullable
+    String zipCode;
+
+    @Schema(
+            example = "Lome"
+    )
+    @NonNull
+    @NotNull
+    String city;
+
+    @Schema(
+            example = "Togo"
+    )
+    @NonNull
+    @NotNull
+    String Land;
+}
